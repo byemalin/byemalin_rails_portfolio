a840c198517c12e788527570281865ea9784ecb4
Malin Mabika <malin@newschool.edu>
2024-04-17 11:01:51 +0200

    removing self ignores from models, as migrations were completed successfully

--- app/models/animation_project.rb
+++ app/models/animation_project.rb
@@ -1,3 +1,2 @@
 class AnimationProject < ApplicationRecord
-  self.ignored_columns += ["cover_image_url", "images"]
 end
--- app/models/programming_project.rb
+++ app/models/programming_project.rb
@@ -1,5 +1,4 @@
 class ProgrammingProject < ApplicationRecord
   # has_one_attached :photo
   has_many_attached :photos
-  self.ignored_columns += ["cover_image_url", "images"]
 end
