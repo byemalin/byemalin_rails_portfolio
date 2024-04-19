59fe88d9fd21bb67c282841dd795ad9b17b54341
Malin Mabika <malin@newschool.edu>
2024-04-19 17:21:49 +0200

    beginning to work with partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "pages#landing"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "pages#landing"
+
+  #pages
+  get "home", to: 'pages#home', as: :home
+
+
 
   resources :programming_projects
   # get "programming_projects", to: 'programming_projects#index'
