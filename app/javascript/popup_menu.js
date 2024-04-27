54c668d5bffe26374a657361d88f16d6316beb0c
Malin Mabika <malin@newschool.edu>
2024-04-27 16:32:09 +0200

    More home page styling and working popup

--- app/javascript/popup_menu.js
+++ app/javascript/popup_menu.js
@@ -1,1 +1,42 @@
 console.log("Popup Javascript Connected");
+
+let greeting = document.getElementById('greetingContainer');
+let popup = document.querySelector('.popupMenuContainer');
+let closeIcon = document.querySelector('.closeIcon');
+
+let i;
+
+greeting.onclick = displayPopup;
+closeIcon.onclick = hidePopup;
+
+function displayPopup(){
+    popup.style.width='50vw';
+}
+
+function hidePopup(){
+    popup.style.width='0px';
+}
+
+
+let closeLines = closeIcon.getElementsByTagName('line');
+
+
+closeIcon.addEventListener('mouseover', iconHover);
+closeIcon.addEventListener('mouseout', iconOut);
+
+
+function iconHover(){
+    closeIcon.style.backgroundColor='#3C3D3E';
+
+    for(i=0; i<2; i++){
+        closeLines[i].style.stroke = '#F4F7FA';
+    }
+}
+
+function iconOut(){
+    closeIcon.style.backgroundColor='#F4F7FA';
+
+    for(i=0; i<2; i++){
+        closeLines[i].style.stroke = '#3C3D3E';
+    }
+}
