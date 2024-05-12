8b13838dda9f2ee4fea58db056a0c02128a3931c
Malin Mabika <malin@newschool.edu>
2024-05-12 15:36:03 +0200

    Removing the bad migration

--- db/migrate/20240512132416_remove_video_url_and_videos_from_animation_projects.rb
+++
@@ -1,6 +0,0 @@
-class RemoveVideoUrlAndVideosFromAnimationProjects < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :animation_projects, :video_url, :string
-    remove_column :animation_projects, :videos, :string, array: true, default: []
-  end
-end
