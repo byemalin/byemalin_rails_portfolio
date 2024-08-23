0386c38fab8560d332e832f2ba4e54d502f0a707
Malin Mabika <malin@newschool.edu>
2024-08-23 17:27:28 +0200

    Adding edit functionality for programming projects

--- app/controllers/programming_projects_controller.rb
+++ app/controllers/programming_projects_controller.rb
@@ -20,7 +20,25 @@ class ProgrammingProjectsController < ApplicationController
     end
   end
 
+
+  before_action :set_programming_project, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    if @programming_project.update(programming_project_params)
+      redirect_to programming_projects_path, notice: 'Programming project was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_programming_project
+    @programming_project = ProgrammingProject.find(params[:id])
+  end
 
   def programming_project_params
     params.require(:programming_project).permit(:title, :description, :technologies_used, :live_url, :tags, :cover_photo, photos: [])
