e966f0d851094eeb1c5b73691408868dfaf81e32
Malin Mabika <malin@newschool.edu>
2024-08-28 17:51:57 +0200

    Adding user authentication to terrain

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:about, :landing, :contact, :home]
   # before_action :authenticate_user!, only: [:dashboard, :profile]
+  before_action :authenticate_user!, only: [:terrain]
 end
