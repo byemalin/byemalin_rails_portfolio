6eb941548aa629630b1d0e36fa63e9888f223388
Malin Mabika <malin@newschool.edu>
2025-06-14 15:18:19 +0200

    Sites can now be made inactive

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_15_155553) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_14_130828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_15_155553) do
     t.string "vimeo_id"
     t.string "video_url"
     t.string "tiktok_id"
+    t.boolean "active", default: true
   end
 
   create_table "users", force: :cascade do |t|
